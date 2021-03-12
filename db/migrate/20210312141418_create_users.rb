642a63535d031c3b57460a12aa59b1b8e63722a7
abeidahmed <abeidahmed92@gmail.com>
2021-03-12 19:51:16 +0530

    Initialize user model and migration

---
+++ db/migrate/20210312141418_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users, id: :uuid do |t|
+      t.string :full_name, null: false, default: ""
+      t.string :email_address, null: false, default: ""
+      t.string :password_digest, null: false, default: ""
+      t.string :auth_token, null: false, default: ""
+
+      t.timestamps
+    end
+
+    add_index :users, :email_address, unique: true
+    add_index :users, :auth_token, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_12_140356) do
+ActiveRecord::Schema.define(version: 2021_03_12_141418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "full_name", default: "", null: false
+    t.string "email_address", default: "", null: false
+    t.string "password_digest", default: "", null: false
+    t.string "auth_token", default: "", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["auth_token"], name: "index_users_on_auth_token", unique: true
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+  end
 
 end
