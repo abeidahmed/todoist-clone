4978f08f59040b6ba0d62b184b71c601d2ae076e
abeidahmed <abeidahmed92@gmail.com>
2021-03-12 23:54:25 +0530

    Style login form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root "static_pages#home"
   resources :users, only: %i[new create]
-  resources :sessions, only: %i[create]
+  resources :sessions, only: %i[new create]
 end
